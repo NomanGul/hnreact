25602a14ad890c21fdf4839e4d53a0b3101a9a5b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-20 15:04:45 +0000

    Deploy armujahid/hnreact to github.com/armujahid/hnreact.git:gh-pages

---
+++ precache-manifest.9691fd0eedb312c84d457e71033ae3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8acd59bed967d41491ec96f45b1341a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ef767fcca47e9af7b91",
+    "url": "/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "f5e12e99dac1dd088ded",
+    "url": "/static/css/main.f88c5b24.chunk.css"
+  },
+  {
+    "revision": "7ef767fcca47e9af7b91",
+    "url": "/static/js/2.223b096b.chunk.js"
+  },
+  {
+    "revision": "f5e12e99dac1dd088ded",
+    "url": "/static/js/main.02b0c41e.chunk.js"
+  },
+  {
+    "revision": "c4d9789b09673756b708",
+    "url": "/static/js/runtime-main.266708ff.js"
+  }
+]);
